19862257aad0adeb7169971ac371950fdf6c6f09
林彭文兵 <zhangqing@pokerlink.cn>
2018-11-01 15:46:30 +0800

    整理

--- Assets/Runtime/Lua/over_buttons.lua
+++ Assets/Runtime/Lua/over_buttons.lua
@@ -1,15 +1,21 @@
 local show_hint = require "hint"
 local upgrade = require "app_upgrade"
+local game = require "game"
 
 local Destroy = UnityEngine.Object.Destroy
 
 local function copy_result(result, data)
+    if not result then
+        return
+    end
+    
     if BDLocationUtil == nil then
         upgrade("app需要更新才能复制战绩")
         return
     end
 
     local time = os.date("%Y-%m-%d %H:%M", data.time)
+    data.type_name = data.type_name or game.get_type_name(data.game_name, data.game_type)
     local share_text = string.format("房间号ID: %d(%d局)\n%s\n%s\n", data.id, data.max_round, time, data.type_name)
 
     for _, v in pairs(result) do
@@ -22,17 +28,13 @@ local function copy_result(result, data)
 end
 
 return function(result, parent, room_data, pos, type, tbl)
+    result = result or ROOM_DATA_SHARE.total_result
+    room_data = room_data or ROOM_DATA_SHARE
+    
     if not parent then
         copy_result(result, room_data)
         return
     end
-
-    room_data = room_data or {
-        time = os.time(),
-        id  = ROOM_DATA_ID,
-        max_round = ROOM_DATA_ROUND,
-        type_name = ROOM_DATA_TYPE,
-    }
 
     local transform = UI.InitPrefab("over_buttons", parent)
 
@@ -42,11 +44,17 @@ return function(result, parent, room_data, pos, type, tbl)
 
     tbl = tbl or {}
     table.insert(tbl, transform)
-    UI.ShareScreen(transform, "share", type, tbl)
-
-    UI.OnClick(transform, "copy", function()
-        copy_result(result, room_data)
-    end)
+    UI.ShareScreen(transform, "share", type, tbl, true)
+    
+    local continue_btn = transform:Find("continue")
+    if UI.Active(continue_btn, UI.room_continue) then
+        UI.OnClick(continue_btn, nil, function()
+            UI.room_continue()
+            UI.room_continue = nil
+        end)
+    end
+    
+    require "feedback"(transform, room_data)
 
     if pos then
         transform.position = pos.position
